b6dcdb263eba47cadd2b26cd6a954d92ad9ab34e
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-28 14:45:50 +0100

    Add other pages and model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { MainContainerComponent } from './main-container/main-container.componen
 import { NavigatorComponent } from './navigator/navigator.component';
 import { ContentContainerComponent } from './content-container/content-container.component';
 import {RouterOutlet} from "@angular/router";
+import { PageMainComponent } from './page-main/page-main.component';
+import { PageProductFormComponent } from './page-product-form/page-product-form.component';
+import { PageProductListComponent } from './page-product-list/page-product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainContainerComponent,
     NavigatorComponent,
-    ContentContainerComponent
+    ContentContainerComponent,
+    PageMainComponent,
+    PageProductFormComponent,
+    PageProductListComponent
   ],
     imports: [
         BrowserModule,
