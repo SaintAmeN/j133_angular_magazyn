ccdd0d638999ad6730d3e7b67464e6e5bea94abd
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-28 14:41:34 +0100

    Szablon skonczony

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    NavigatorComponent,
+    ContentContainerComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
